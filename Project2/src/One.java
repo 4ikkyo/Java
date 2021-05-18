946f9c06ad48609f22452dc49df1019f662e17b5
Probel <makssonyt@gmail.com>
2021-05-18 12:51:46 +0300

    2

--- Project2/src/One.java
+++ Project2/src/One.java
@@ -2,23 +2,60 @@ import java.io.*;
 
 public class One 
 {
-
     public static void main(String[] args) 
     throws IOException 
     {
-        try
+        try 
         {
-            FileInputStream fileInputStream = new FileInputStream("C:\\VSCode\\Java\\Project2\\src\\a2.txt");
-            int i;
-            while((i=fileInputStream.read())!= -1)
-            {
-                System.out.print((int)i);
-            }
-        }   
+        BufferedReader in = new BufferedReader(new FileReader("C:\\VSCode\\Java\\Project2\\src\\a1.txt"));
+        String s = in.readLine();
+        String[] strScores = s.split(" ");
+        int sum1 = 0;
+        int sum2 = 0;
+        double h1=0;
+        double h2=0;
+        int[] x = new int[30];
+        int[] y = new int[30];
+        
+        for(int j=0;j<30;j++){                      //ввод чисел в массив x
+            x[j] = Integer.parseInt(strScores[j]);
+        }
+
+        for(int j=30,i=0;i<30;j++,i++){             //ввод чисел в массив y
+            y[i] = Integer.parseInt(strScores[j]);
+        }
+        
+        System.out.print("Массив x: ");
+        
+        for (int j = 0; j < x.length; j++) {        //вывод массив x
+            System.out.print(x[j] + " ");
+        }
+
+        System.out.print("\nМассив y: ");
+        
+        for (int j = 0; j < y.length; j++) {       //вывод массив y
+            System.out.print(y[j] + " ");
+        }
+        
+        for (int i = 0; i < x.length; i++, i++) { //счет x1*y1+x3*y3+x5*y5...
+            sum1 = x[i] * y[i];
+            h1 += sum1;
+        }
+
+        System.out.println("\n\nx1*y1+x3*y3+x5*y5... = "+h1);
+
+        for (int i = 1; i < y.length; i++, i++) { //счет x2*y2+x4*y4+x6*y6...
+            sum2 = x[i]*y[i];
+            h2 += sum2;
+        }
+
+        System.out.println("x2*y2+x4*y4+x6*y6... = "+h2);
+    
+        System.out.println("------------------------------\n"+h1 / h2); //Вывод
+        }
         catch (FileNotFoundException e)
         {
             System.out.println("Файл не надйен");
         }
-
     }
 }
--- Project2/src/Two.java
+++ Project2/src/Two.java
@@ -1,18 +1,60 @@
-import java.io.*;  
+import java.io.*;
 
-class Two {  
-    public static void main(String args[]) 
+public class Two {
+        public static void main(String[] args) 
+        throws IOException 
+        {
+            try 
+            {
+            BufferedReader in = new BufferedReader(new FileReader("C:\\VSCode\\Java\\Project2\\src\\a1.txt"));
+            String s = in.readLine();
+            String[] strScores = s.split(" ");
+            int sum1 = 0;
+            int sum2 = 0;
+            double h1=0;
+            double h2=0;
+            int[] x = new int[30];
+            int[] y = new int[30];
+            
+            for(int j=0,i=0;i<30;j++,j++,i++){                      //ввод чисел в массив x
+                x[i] = Integer.parseInt(strScores[j]);
+            }
     
-    throws IOException  
-    {    
-        char c;  
-        BufferedReader br  = new BufferedReader(new InputStreamReader (System.in));  
-        System.out.print("Введіть символи, 'q' для виходу.");  
-        // читати символи  
-        do 
-        {  
-            c = (char) br.read();  
-            System.out.println(c);
-        }   while(c != 'q');   
-    }
+            for(int j=1,i=0;i<30;j++,j++,i++){             //ввод чисел в массив y
+                y[i] = Integer.parseInt(strScores[j]);
+            }
+            
+            System.out.print("Массив x: ");
+            
+            for (int j = 0; j < x.length; j++) {        //вывод массив x
+                System.out.print(x[j] + " ");
+            }
+    
+            System.out.print("\nМассив y: ");
+            
+            for (int j = 0; j < y.length; j++) {       //вывод массив y
+                System.out.print(y[j] + " ");
+            }
+            
+            for (int i = 0; i < x.length; i++, i++) { //счет x1*y1+x3*y3+x5*y5...
+                sum1 = x[i] * y[i];
+                h1 += sum1;
+            }
+    
+            System.out.println("\n\nx1*y1+x3*y3+x5*y5... = "+h1);
+    
+            for (int i = 1; i < y.length; i++, i++) { //счет x2*y2+x4*y4+x6*y6...
+                sum2 = x[i]*y[i];
+                h2 += sum2;
+            }
+    
+            System.out.println("x2*y2+x4*y4+x6*y6... = "+h2);
+        
+            System.out.println("------------------------------\n"+h1 / h2); //Вывод
+            }
+            catch (FileNotFoundException e)
+            {
+                System.out.println("Файл не надйен");
+            }
+        }
 }
