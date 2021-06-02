b8c5857ada9be955a327e2615ab36e1256e1d151
Probel <makssonyt@gmail.com>
2021-06-02 21:49:57 +0300

    new

--- Project1/src/App.java
+++ Project1/src/App.java
@@ -1,5 +1,7 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        int a = 5;
+        System.out.println("Периметр квадрата = " + 4 * a);
+        System.out.println("Площадь квадрата = " + a * a);
     }
 }
