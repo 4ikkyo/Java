936110a74aa6a5972d6dcbda288f819f9f32c56e
Казаков Влад <fresh2017ick@gmail.com>
2021-06-10 23:11:16 +0300

    new

--- Project1/src/SredneArifm2.java
+++ Project1/src/SredneArifm2.java
@@ -1,6 +1,6 @@
 public class SredneArifm2 {
     public static void main (String args[]){
-        int a=-1, b=-2, c=-3;
+        int a=1, b=2, c=3;
         System.out.print("Среднее арифметическое:" + (a+b+c)/3);
     }
 }
