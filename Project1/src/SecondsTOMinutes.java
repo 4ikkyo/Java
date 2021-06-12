ff42880d3d7ddeb51f4bf5171faf663c36a07164
Максим Сновида <snovida2003@gmail.com>
2021-06-13 01:25:15 +0300

    new

--- Project1/src/SecondsTOMinutes.java
+++ Project1/src/SecondsTOMinutes.java
@@ -1,6 +1,6 @@
 public class SecondsTOMinutes {
     public static void main (String args[]){
-        float N = 120;
+        int N = 120;
         System.out.print("З початку доби пройшло " + (N/60) + " хвилини");
     }
 }
