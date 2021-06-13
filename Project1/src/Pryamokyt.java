25a569c9a7f80352834c5a5a203c2eaae69c9161
Максим Сновида <snovida2003@gmail.com>
2021-06-13 17:15:48 +0300

    new

---
+++ Project1/src/Pryamokyt.java
@@ -0,0 +1,8 @@
+public class Pryamokyt {
+    public static void main (String args[])
+    {
+        int a=4,b=8;
+        System.out.println("Площадь прямоуглоьника= "+(a*b));
+        System.out.println("Периметр прямоуглоьника= "+(2*(a+b)));
+    }
+}
